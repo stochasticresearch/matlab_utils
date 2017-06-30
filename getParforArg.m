f6eed765d2d5693038df13f0b3b93410a7b7e8fa
Kiran Karra <kiran.karra@gmail.com>
2017-06-30 13:29:37 -0400

    adding function for parfor ease

---
+++ getParforArg.m
@@ -0,0 +1,9 @@
+% Create a helper function to choose the NumWorkers argument to PARFOR
+function arg = getParforArg()
+    p = gcp('nocreate'); % get the pool object if it exists, but never open a pool
+    if isempty(p)
+        arg = 0;
+    else
+        arg = p.NumWorkers;
+    end
+end
